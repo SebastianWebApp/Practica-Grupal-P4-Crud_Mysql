dd504dab087e59a328f59c5b5ee101ca2bd76c13
Mateo Espinosa <mateoespinosam@hotmail.com>
2025-05-13 15:27:03 -0500

    Cambio

--- Database/conectar.js
+++ Database/conectar.js
@@ -7,8 +7,7 @@ const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    port: process.env.PORT1,
-    database: process.env.DATABASE
+    port: process.env.PORT1
 });
 
 export default pool;
